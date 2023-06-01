5926fdc5ddb04fede17f332c1a8c97f78dfd28de
Carlos <andre021297@gmail.com>
2023-06-01 03:00:48 -0500

    ?

--- api/serializers/reservation_serializers.py
+++ api/serializers/reservation_serializers.py
@@ -1,6 +1,22 @@
 from rest_framework import serializers
 from api.models import Reservation,Buoys
-from api.serializers import BuoysSerializer
+from api.serializers import ReservationSerializer
+
+class BuoysSerializer(serializers.ModelSerializer):
+    reservations = ReservationSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Buoys
+        fields = ['id', 'latitude', 'longitude', 'size', 'created_at', 'updated_at', 'reservations', 'price1', 'price2']
+        read_only_fields = ['price1', 'price2']
+
+    def create(self, validated_data):
+        # Remove the price1 and price2 fields from the validated data
+        validated_data.pop('price1', None)
+        validated_data.pop('price2', None)
+
+        return super().create(validated_data)
+    
 class ReservationSerializer(serializers.ModelSerializer):
     buoy = BuoysSerializer(read_only=True)  # Use BuoysSerializer
     buoy_id = serializers.PrimaryKeyRelatedField(source='buoy', queryset=Buoys.objects.all(), write_only=True)
